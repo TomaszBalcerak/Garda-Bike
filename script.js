7abe358a24d1ae8b2e2caf1bb0609f190a668d89
Tomasz Balcerak <tomasz.patryk.balcerak@gmail.com>
2019-09-05 22:35:32 +0200

    Trick with js innerHeight to avoid jumping page on mobile v2

--- script.js
+++ script.js
@@ -9,6 +9,15 @@ $("#menu-button").click(function(){
   $('.modal').css("display", "none");
 });
 
+//Usunięcie defektu skaczącego widoku na mobile//
+// We listen to the resize event
+window.addEventListener('resize', () => {
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
 //zamykanie menu gdy kliknie sie gdziekolwiek//
 
 const $menu = $('#menu-button');
